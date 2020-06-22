4b4603d2987e32bed7c5b542326f7c99818fa2de
wangxin <wasim37@163.com>
2020-06-22 17:21:07 +0800

    commit

---
+++ 图书文本分类/test.py
@@ -0,0 +1,22 @@
+from gensim import models
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.externals import joblib
+import gensim
+import sys
+import pandas as pd
+import matplotlib
+import numpy as np
+import scipy as sp
+import IPython
+from IPython import display
+import sklearn
+
+print("Python version: {}".format(gensim.__version__))
+print("Python version: {}".format(sys.version))
+print("pandas version: {}".format(pd.__version__))
+print("matplotlib version: {}".format(matplotlib.__version__))
+print("NumPy version: {}".format(np.__version__))
+print("SciPy version: {}".format(sp.__version__))
+print("IPython version: {}".format(IPython.__version__))
+print("scikit-learn version: {}".format(sklearn.__version__))
+
